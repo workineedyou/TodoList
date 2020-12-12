62ed82a8d7b6c2169bfcd93af0a886b7dde405bf
Viktor <viktor.fd.84@gmail.com>
2020-12-12 19:35:47 +0300

    delete items with btn click

--- src/components/Data/todosData.js
+++
@@ -1,7 +0,0 @@
-const todosData = [
-    { id: 1, label: 'Drink Coffee', important: false },
-    { id: 2, label: 'Learn React', important: false },
-    { id: 3, label: 'Create React Application', important: true },
-]
-
-export default todosData
